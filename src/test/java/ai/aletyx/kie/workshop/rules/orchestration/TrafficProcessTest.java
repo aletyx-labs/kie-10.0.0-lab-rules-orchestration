0f248e5fb2c16cf2fed514441094f0710565ccbb
Alex Porcelli <alex@porcelli.me>
2025-02-20 17:36:39 -0500

    expected result

---
+++ src/test/java/ai/aletyx/kie/workshop/rules/orchestration/TrafficProcessTest.java
@@ -0,0 +1,41 @@
+package ai.aletyx.kie.workshop.rules.orchestration;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+public class TrafficProcessTest {
+
+    public static final BigDecimal SPEED_LIMIT = new BigDecimal(100);
+
+    @Test
+    public void testTrafficViolationEmbeddedDecisionOnQuarkus() {
+        testTrafficProcess("traffic", "12345", 120d, "No", true);
+        testTrafficProcess("traffic", "12345", 140d, "Yes", true);
+        testTrafficProcess("traffic", "1234", 140d, null, false);
+    }
+
+    private void testTrafficProcess(String processId, String driverId, Double speed, String suspended, Boolean validLicense) {
+        Map<String, Object> request = new HashMap<>();
+        request.put("driverId", driverId);
+        request.put("violation", new Violation("speed", SPEED_LIMIT, new BigDecimal(speed)));
+        given()
+                .body(request)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/" + processId)
+                .then()
+                .statusCode(201)
+                .body("suspended", is(suspended))
+                .body("driver.validLicense", is(validLicense));
+    }
+}
