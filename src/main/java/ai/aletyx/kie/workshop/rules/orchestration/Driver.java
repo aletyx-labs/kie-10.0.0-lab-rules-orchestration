66b40a36aff073340bf32a73dbc1b2a6769b4806
Tim Wuthenow <tim.wuthenow@gmail.com>
2025-03-18 10:57:18 -0400

    Adding some changes to make date time a part of DMN

--- src/main/java/ai/aletyx/kie/workshop/rules/orchestration/Driver.java
+++ src/main/java/ai/aletyx/kie/workshop/rules/orchestration/Driver.java
@@ -8,105 +8,106 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Driver {
 
-    private String id;
+	private String id;
 
-    @JsonProperty("Name")
-    private String name;
+	@JsonProperty("Name")
+	private String name;
 
-    @JsonProperty("State")
-    private String state;
+	@JsonProperty("State")
+	private String state;
 
-    @JsonProperty("City")
-    private String city;
+	@JsonProperty("City")
+	private String city;
 
-    @JsonProperty("Points")
-    private Integer points;
+	@JsonProperty("Points")
+	private Integer points;
 
-    @JsonProperty("Age")
-    private Integer age;
+	@JsonProperty("Age")
+	private Integer age;
 
-    private Date licenseExpiration;
+	private Date licenseExpiration;
 
-    private Boolean validLicense = Boolean.FALSE;
+	private Boolean validLicense = Boolean.FALSE;
 
-    public Driver() {
-    }
+	public Driver() {
+	}
 
-    public Driver(String id, String name, String state, String city, Integer points, Integer age, Date licenseExpiration) {
-        this.id = id;
-        this.name = name;
-        this.state = state;
-        this.city = city;
-        this.points = points;
-        this.age = age;
-        this.licenseExpiration = licenseExpiration;
-    }
+	public Driver(String id, String name, String state, String city, Integer points, Integer age,
+			Date licenseExpiration) {
+		this.id = id;
+		this.name = name;
+		this.state = state;
+		this.city = city;
+		this.points = points;
+		this.age = age;
+		this.licenseExpiration = licenseExpiration;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getState() {
-        return state;
-    }
+	public String getState() {
+		return state;
+	}
 
-    public void setState(String state) {
-        this.state = state;
-    }
+	public void setState(String state) {
+		this.state = state;
+	}
 
-    public String getCity() {
-        return city;
-    }
+	public String getCity() {
+		return city;
+	}
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+	public void setCity(String city) {
+		this.city = city;
+	}
 
-    public Integer getPoints() {
-        return points;
-    }
+	public Integer getPoints() {
+		return points;
+	}
 
-    public void setPoints(Integer points) {
-        this.points = points;
-    }
+	public void setPoints(Integer points) {
+		this.points = points;
+	}
 
-    public Integer getAge() {
-        return age;
-    }
+	public Integer getAge() {
+		return age;
+	}
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 
-    public Date getLicenseExpiration() {
-        return licenseExpiration;
-    }
+	public Date getLicenseExpiration() {
+		return licenseExpiration;
+	}
 
-    public void setLicenseExpiration(Date licenseExpiration) {
-        this.licenseExpiration = licenseExpiration;
-    }
+	public void setLicenseExpiration(Date licenseExpiration) {
+		this.licenseExpiration = licenseExpiration;
+	}
 
-    public Boolean isValidLicense() {
-        return validLicense;
-    }
+	public Boolean isValidLicense() {
+		return validLicense;
+	}
 
-    public Boolean getValidLicense() {
-        return validLicense;
-    }
+	public Boolean getValidLicense() {
+		return validLicense;
+	}
 
-    public void setValidLicense(Boolean validLicense) {
-        this.validLicense = validLicense;
-    }
+	public void setValidLicense(Boolean validLicense) {
+		this.validLicense = validLicense;
+	}
 }
--- src/main/java/ai/aletyx/kie/workshop/rules/orchestration/Violation.java
+++ src/main/java/ai/aletyx/kie/workshop/rules/orchestration/Violation.java
@@ -1,74 +1,76 @@
 package ai.aletyx.kie.workshop.rules.orchestration;
 
 import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Violation {
 
-    @JsonProperty("Code")
-    private String code;
+	@JsonProperty("Code")
+	private String code;
 
-    @JsonProperty("Date")
-    private Date date;
+	@JsonProperty("Date")
+	private OffsetDateTime date;
 
-    @JsonProperty("Type")
-    private String type;
+	@JsonProperty("Type")
+	private String type;
 
-    @JsonProperty("Speed Limit")
-    private BigDecimal speedLimit;
+	@JsonProperty("Speed Limit")
+	private BigDecimal speedLimit;
 
-    @JsonProperty("Actual Speed")
-    private BigDecimal actualSpeed;
+	@JsonProperty("Actual Speed")
+	private BigDecimal actualSpeed;
 
-    public Violation() {
-    }
+	public Violation() {
+	}
 
-    public Violation(String type, BigDecimal speedLimit, BigDecimal actualSpeed) {
-        this.type = type;
-        this.speedLimit = speedLimit;
-        this.actualSpeed = actualSpeed;
-        this.date = new Date();
-    }
+	public Violation(String code, OffsetDateTime date, String type, BigDecimal speedLimit, BigDecimal actualSpeed) {
+		this.code = code;
+		this.date = date;
+		this.type = type;
+		this.speedLimit = speedLimit;
+		this.actualSpeed = actualSpeed;
+	}
 
-    public String getCode() {
-        return code;
-    }
+	public String getCode() {
+		return code;
+	}
 
-    public void setCode(String code) {
-        this.code = code;
-    }
+	public void setCode(String code) {
+		this.code = code;
+	}
 
-    public Date getDate() {
-        return date;
-    }
+	public OffsetDateTime getDate() {
+		return date;
+	}
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
+	public void setDate(OffsetDateTime date) {
+		this.date = date;
+	}
 
-    public String getType() {
-        return type;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public void setType(String type) {
-        this.type = type;
-    }
+	public void setType(String type) {
+		this.type = type;
+	}
 
-    public BigDecimal getSpeedLimit() {
-        return speedLimit;
-    }
+	public BigDecimal getSpeedLimit() {
+		return speedLimit;
+	}
 
-    public void setSpeedLimit(BigDecimal speedLimit) {
-        this.speedLimit = speedLimit;
-    }
+	public void setSpeedLimit(BigDecimal speedLimit) {
+		this.speedLimit = speedLimit;
+	}
 
-    public BigDecimal getActualSpeed() {
-        return actualSpeed;
-    }
+	public BigDecimal getActualSpeed() {
+		return actualSpeed;
+	}
 
-    public void setActualSpeed(BigDecimal actualSpeed) {
-        this.actualSpeed = actualSpeed;
-    }
+	public void setActualSpeed(BigDecimal actualSpeed) {
+		this.actualSpeed = actualSpeed;
+	}
 }
