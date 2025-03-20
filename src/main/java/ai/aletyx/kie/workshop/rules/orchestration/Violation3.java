8306d0dab228d1ec238c78b41f6da3010088c1f1
Tim Wuthenow <tim.wuthenow@gmail.com>
2025-03-19 21:06:39 -0400

    3 scenarios all cohabitating

--- src/main/java/ai/aletyx/kie/workshop/rules/orchestration/Violation3.java
+++ src/main/java/ai/aletyx/kie/workshop/rules/orchestration/Violation3.java
@@ -7,16 +7,19 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Violation {
+public class Violation3 {
 
 	@JsonProperty("Code")
 	private String code;
 
+	@JsonProperty("Date")
+	private OffsetDateTime date;
+
 	// @JsonProperty("Date")
 	// private LocalDate date;
 
-	@JsonProperty("Date")
-	private Date date;
+	// @JsonProperty("Date")
+	// private Date date;
 
 	@JsonProperty("Type")
 	private String type;
@@ -27,27 +30,10 @@ public class Violation {
 	@JsonProperty("Actual Speed")
 	private BigDecimal actualSpeed;
 
-	public Violation() {
+	public Violation3() {
 	}
 
-	// public Violation(String code, LocalDate date, String type, BigDecimal
-	// speedLimit, BigDecimal actualSpeed) {
-	// this.code = code;
-	// this.date = date;
-	// this.type = type;
-	// this.speedLimit = speedLimit;
-	// this.actualSpeed = actualSpeed;
-	// }
-
-	// public LocalDate getDate() {
-	// return date;
-	// }
-
-	// public void setDate(LocalDate date) {
-	// this.date = date;
-	// }
-
-	public Violation(String code, Date date, String type, BigDecimal speedLimit, BigDecimal actualSpeed) {
+	public Violation3(String code, OffsetDateTime date, String type, BigDecimal speedLimit, BigDecimal actualSpeed) {
 		this.code = code;
 		this.date = date;
 		this.type = type;
@@ -55,13 +41,30 @@ public class Violation {
 		this.actualSpeed = actualSpeed;
 	}
 
-	public Date getDate() {
+	public OffsetDateTime getDate() {
 		return date;
 	}
 
-	public void setDate(Date date) {
+	public void setDate(OffsetDateTime date) {
 		this.date = date;
 	}
+
+	// public Violation(String code, Date date, String type, BigDecimal speedLimit,
+	// BigDecimal actualSpeed) {
+	// this.code = code;
+	// this.date = date;
+	// this.type = type;
+	// this.speedLimit = speedLimit;
+	// this.actualSpeed = actualSpeed;
+	// }
+
+	// public Date getDate() {
+	// return date;
+	// }
+
+	// public void setDate(Date date) {
+	// this.date = date;
+	// }
 
 	public String getCode() {
 		return code;
